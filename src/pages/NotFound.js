ba8e823b5d75a700c76be0d71825a24f02730ab4
Reha Gursel <gurselreha@gmail.com>
2022-06-22 05:39:13 +0300

    github pages update

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
     <div className="mx-auto text-center pt-20">
       <p>Page not found!</p>
+      <Link
+          to="/products"
+          className="flex items-center justify-center w-32 h-20 text-xl sm:text-2xl sm:w-56  bg-black text-white font-bold mx-auto mt-20"
+        >
+          Home Page
+        </Link>
     </div>
   );
 };
